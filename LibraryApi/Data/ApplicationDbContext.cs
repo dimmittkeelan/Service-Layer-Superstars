5508e04bb92427bb821003976fdce5f61cfce0e0
owinrojas <owin@owinrojas.com>
2026-04-26 14:57:53 -0700

    Add database-level constraints

--- LibraryApi/Data/ApplicationDbContext.cs
+++ LibraryApi/Data/ApplicationDbContext.cs
@@ -7,8 +7,22 @@ namespace LibraryApi.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext>
         options) : base(options) { }
+
         public DbSet<Book> Books => Set<Book>();
         public DbSet<Member> Members => Set<Member>();
         public DbSet<BorrowRecord> BorrowRecords => Set<BorrowRecord>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Book>()
+                .ToTable(tableBuilder =>
+                {
+                    tableBuilder.HasCheckConstraint("CK_Books_TotalCopies_Positive", "TotalCopies > 0");
+                    tableBuilder.HasCheckConstraint("CK_Books_AvailableCopies_NonNegative", "AvailableCopies >= 0");
+                    tableBuilder.HasCheckConstraint("CK_Books_AvailableCopies_NotGreaterThanTotal", "AvailableCopies <= TotalCopies");
+                });
+        }
     }
 }
